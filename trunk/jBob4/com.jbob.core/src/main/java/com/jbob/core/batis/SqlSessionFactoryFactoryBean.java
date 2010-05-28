afc70cfe7f57d674f6c7b3359199b8fa15a0d1ce
chenjpu <chenjpu@5ed34be0-4c84-cabf-3d81-dc756c19627b>
2010-05-28 09:03:55 +0000

    添加ehcache

--- trunk/jBob4/com.jbob.core/src/main/java/com/jbob/core/batis/SqlSessionFactoryFactoryBean.java
+++ trunk/jBob4/com.jbob.core/src/main/java/com/jbob/core/batis/SqlSessionFactoryFactoryBean.java
@@ -3,10 +3,13 @@ package com.jbob.core.batis;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.sql.DataSource;
+
+import net.sf.ehcache.CacheManager;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -17,23 +20,29 @@ import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.managed.ManagedTransactionFactory;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.util.Assert;
 
-public class SqlSessionFactoryFactoryBean implements FactoryBean<SqlSessionFactory>,InitializingBean{
+import com.jbob.core.batis.cache.BatisEhCache;
+
+public class SqlSessionFactoryFactoryBean implements FactoryBean<SqlSessionFactory>,InitializingBean, DisposableBean {
 	Log logger = LogFactory.getLog(SqlSessionFactoryFactoryBean.class);
+	private Resource cacheLocation;
 	private Resource configLocation;
 	private Resource[] mapperLocations;
 	private DataSource dataSource;
 	private boolean useTransactionAwareDataSource = true;
-	
-	SqlSessionFactory sqlSessionFactory;
+	private SqlSessionFactory sqlSessionFactory;
+	private CacheManager cacheManager;
 	public void afterPropertiesSet() throws Exception {
 		Assert.notNull(configLocation,"configLocation must be not null");
-		
+		if (this.cacheLocation != null) {
+			this.cacheManager = new CacheManager(this.cacheLocation.getInputStream());
+		}
 		sqlSessionFactory = createSqlSessionFactory();
 	}
 
@@ -47,9 +56,17 @@ public class SqlSessionFactoryFactoryBean implements FactoryBean<SqlSessionFacto
 				if (this.useTransactionAwareDataSource  && !(this.dataSource instanceof TransactionAwareDataSourceProxy)) {  
 		            dataSourceToUse = new TransactionAwareDataSourceProxy(this.dataSource);  
 		        }
-				
 				conf.setEnvironment(new Environment("development",new ManagedTransactionFactory(),dataSourceToUse));
-				sqlSessionFactory = new SqlSessionFactoryBuilder().build(conf);
+				
+				if(cacheManager != null){
+					String[] cacheNames= cacheManager.getCacheNames();
+					
+					System.out.println(Arrays.toString(cacheNames));
+					for (String id : cacheNames) {
+						conf.addCache(new BatisEhCache(cacheManager.getEhcache(id)));
+					}
+				}
+				//sqlSessionFactory = new SqlSessionFactoryBuilder().build(conf);
 			}
 			
 			if(mapperLocations != null) {
@@ -103,5 +120,19 @@ public class SqlSessionFactoryFactoryBean implements FactoryBean<SqlSessionFacto
 	public void setMapperLocations(Resource[] mapperLocations) {
 		this.mapperLocations = mapperLocations;
 	}
+
+	public Resource getCacheLocation() {
+		return cacheLocation;
+	}
+
+	public void setCacheLocation(Resource cacheLocation) {
+		this.cacheLocation = cacheLocation;
+	}
+	
+	public void destroy() {
+		logger.info("Shutting down EHCache CacheManager");
+		//this.cacheManager.shutdown();
+	}
+
 
 }
